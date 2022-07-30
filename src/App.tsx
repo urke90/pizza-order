c0c9d7a8d792a326d3674ee79d59e42971596b9b
urke90 <urosbijelic90@gmail.com>
2022-07-30 11:21:03 +0200

    converted firebase to ts file, removed AuthContext that was for testing purpose

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,22 @@ import { Routes, Route } from 'react-router-dom';
 
 import Main from 'pages/Main';
 import Login from 'pages/Login';
+import { getAuth } from 'firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth';
 
 import './App.scss';
 
 function App() {
+    const auth = getAuth();
+    console.log('auth', auth);
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            console.log('user imaaaa', user);
+        } else {
+            // user is signed out
+        }
+    });
+
     return (
         <div className="App">
             <Routes>

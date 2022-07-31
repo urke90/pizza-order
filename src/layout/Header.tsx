eff610d445719495b2ebb4fd53be1040f71dd8df
urke90 <urosbijelic90@gmail.com>
2022-07-31 11:15:33 +0200

    header component with navigation--- WIP--- click outside menu should close it, check for height of navlinks and logout btn

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { GiFullPizza } from 'react-icons/gi';
+import { HiMenu } from 'react-icons/hi';
 
 import NavLinkCustom from 'shared/links/NavLink';
 import Button from 'shared/form/Button';
@@ -30,6 +31,11 @@ const NAVLINKS_CONFIG: INavLinksConfig[] = [
 ];
 
 const Header: React.FC<HeaderProps> = () => {
+    const [openedMenu, setOpenedMenu] = useState(false);
+    const menu = useRef<HTMLUListElement>(null);
+
+    const handleOpenMenu = () => setOpenedMenu(!openedMenu);
+
     return (
         <div className="header">
             <div className="header__container">
@@ -44,14 +50,54 @@ const Header: React.FC<HeaderProps> = () => {
                             const { to, text } = navLink;
                             return (
                                 <li className="header__item">
-                                    <NavLinkCustom to={to} text={text} />
+                                    <NavLinkCustom
+                                        key={to}
+                                        to={to}
+                                        text={text}
+                                    />
                                 </li>
                             );
                         })}
                     </ul>
-                    <Button type="button" onClick={() => {}}>
-                        logout
-                    </Button>
+                    <div className="header__btn--logout">
+                        <Button type="button" onClick={() => {}}>
+                            logout
+                        </Button>
+                    </div>
+                    <HiMenu
+                        className="header__menu"
+                        size={50}
+                        color="#feb139"
+                        onClick={handleOpenMenu}
+                    />
+                    {openedMenu && (
+                        <ul
+                            className="header__navigation-list--mobile"
+                            ref={menu}
+                        >
+                            {NAVLINKS_CONFIG.map((navLink) => {
+                                const { to, text } = navLink;
+                                return (
+                                    <li className="header__item--mobile">
+                                        <NavLinkCustom
+                                            key={to}
+                                            to={to}
+                                            text={text}
+                                        />
+                                    </li>
+                                );
+                            })}
+                            <li className="header__item--mobile">
+                                <Button
+                                    type="button"
+                                    width={160}
+                                    onClick={() => {}}
+                                >
+                                    logout
+                                </Button>
+                            </li>
+                        </ul>
+                    )}
                 </nav>
             </div>
         </div>
--- src/shared/links/NavLink.tsx
+++ src/shared/links/NavLink.tsx
@@ -14,7 +14,8 @@ const NavLinkCustom: React.FC<NavLinkProps> = ({ text, to }) => {
             style={({ isActive }) => {
                 return {
                     color: isActive ? '#001e28' : '#feb139',
-                    backgroundColor: isActive ? '#feb139' : '#001e28'
+                    backgroundColor: isActive ? '#feb139' : '#001e28',
+                    padding: '6px 10px'
                 };
             }}
         >

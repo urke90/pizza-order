d4c288f10e2bf3cf7d63153bfd94d7bf27599ce6
urke90 <urosbijelic90@gmail.com>
2022-08-01 15:36:23 +0200

    split routes to different file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import RoutesComponent from 'router/Routes';
-import Main from 'pages/Main';
-import Login from 'pages/Login';
+
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase/firebase';
 import Header from 'layout/Header';
-import Orders from 'pages/Orders';
-import CustomPizza from 'pages/CustomPizza';
 
 import './App.scss';
 
@@ -26,12 +23,7 @@ function App() {
     return (
         <div>
             {pathname !== '/login' && <Header />}
-            {/* <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="login" element={<Login />} />
-                <Route path="orders" element={<Orders />} />
-                <Route path="custom-piza" element={<CustomPizza />} />
-            </Routes> */}
+
             <RoutesComponent />
         </div>
     );

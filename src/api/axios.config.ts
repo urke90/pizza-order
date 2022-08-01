437a3a6a571663411ec049e1275dc1e2aac35c70
urke90 <urosbijelic90@gmail.com>
2022-08-01 10:28:40 +0200

    removed base url from axios instance

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: 'https://pizza-order-6d2f9-default-rtdb.firebaseio.com/'
+    //
     // timeout: 1000,
     // headers: {'X-Custom-Header': 'foobar'}
 });

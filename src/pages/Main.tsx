2c70bbe51a8569d31d7516be6cf8ecb6248312ac
urke90 <urosbijelic90@gmail.com>
2022-08-02 15:27:10 +0200

    made small html structure and style tweaks

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useAxios } from 'hooks/useAxios';
-
 import { API_ENDPOINTS } from 'api/endpoints';
+
+import LoadingSpinner from 'shared/ui/LoadingSpinner';
 
 import './Main.scss';
 
 type MainProps = {};
 
 const Main: React.FC<MainProps> = () => {
-    const { sendRequest } = useAxios();
+    const { sendRequest, isLoading, error } = useAxios();
     const [recId, setRecId] = useState();
 
     useEffect(() => {
@@ -23,33 +24,32 @@ const Main: React.FC<MainProps> = () => {
         fetchPizzas();
     }, []);
 
-    useEffect(() => {
-        const fetchPizzas = async () => {
-            const url = API_ENDPOINTS.pizza;
+    // useEffect(() => {
+    //     const fetchPizzas = async () => {
+    //         const url = API_ENDPOINTS.pizza;
 
-            const response = await sendRequest({
-                url: 'https://forkify-api.herokuapp.com/api/get?rId=35477',
-                method: 'GET'
-            });
-            console.log('response RECIPE', response);
-        };
+    //         const response = await sendRequest({
+    //             url: 'https://forkify-api.herokuapp.com/api/get?rId=35477',
+    //             method: 'GET'
+    //         });
+    //         console.log('response RECIPE', response);
+    //     };
 
-        fetchPizzas();
-    }, []);
+    //     fetchPizzas();
+    // }, []);
 
     return (
         <section className="main">
+            {isLoading && !error && <LoadingSpinner asOverlay />}
             <div className="main__container">
                 <div className="main__heading-wrapper">
                     <h2 className="main__heading">Main Pizza Page</h2>
                     <p>Pick your favorite pizza</p>
                 </div>
                 <div className="main__content">
-                    <aside className="main__menu">menu here</aside>
-                    <main>present recepies</main>
-                    <aside className="main__ingredients">
-                        ingredients here
-                    </aside>
+                    <div className="main__pizza-menu a">menu here</div>
+                    <main className="main__recipe b">present recepies</main>
+                    <div className="main__ingredients  c">ingredients here</div>
                 </div>
             </div>
         </section>

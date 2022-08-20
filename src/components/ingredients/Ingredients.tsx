ddc2820a4439d87026f73e7b0d0626d8cb38a853
urke90 <urosbijelic90@gmail.com>
2022-08-20 14:01:15 +0200

    create btns for choosing value of inc/dec ingredients

--- src/components/ingredients/Ingredients.tsx
+++ src/components/ingredients/Ingredients.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+
+import Button from 'shared/form/Button';
+import './Ingredients.scss';
 
 import Input from 'shared/form/Input';
 
@@ -10,7 +13,30 @@ import { IConvertedIngredients } from 'ts/ingredients';
 
 const Ingredients: React.FC = ({}) => {
     // console.log('ingredients in Ingredients component', ingredients);
+    const [ingValueConstant, setIngValueConstant] = useState<number>(0.25);
+    const handleConstantValueChange = (value: number) =>
+        setIngValueConstant(value);
 
-    return <div>Ingredients component</div>;
+    return (
+        <div className="ingredients">
+            <div className="ingredients__heading">
+                <p>Increment or decrement by:</p>
+                <div className="ingredients__buttons-wrapper">
+                    <Button
+                        type="button"
+                        onClick={() => handleConstantValueChange(0.25)}
+                    >
+                        0.25
+                    </Button>
+                    <Button
+                        type="button"
+                        onClick={() => handleConstantValueChange(1)}
+                    >
+                        1
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 };
 export default Ingredients;

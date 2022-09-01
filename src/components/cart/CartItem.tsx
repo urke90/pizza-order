6dbcec87477a131bae6ec91e412028d53ad6afdf
urke90 <urosbijelic90@gmail.com>
2022-09-01 19:19:58 +0200

    added CartItem component with cart-ingredient compoenent and styled

---
+++ src/components/cart/CartItem.tsx
@@ -0,0 +1,51 @@
+import { ICartItem } from 'ts/orders';
+
+import CartIngredientItem from './CartIngredientItem';
+
+import './CartItem.scss';
+
+interface ICartItemProps {
+    item: ICartItem;
+}
+
+const CartItem: React.FC<ICartItemProps> = ({ item }) => {
+    // console.log('item in cartItem', item);
+
+    const { id, imageUrl, title, sourceUrl, quantity, ingredients } = item;
+
+    const ingredientsToRender = Object.values(ingredients);
+
+    // console.log('ingredientsToRender', ingredientsToRender);
+
+    return (
+        <li className="cart-item">
+            <div className="cart-item__heading">
+                <div className="cart-item__img">
+                    <img src={imageUrl} alt={title} />
+                </div>
+                <div className="cart-item__description">
+                    <h5 className="cart-item__title">{title}</h5>
+                    <a
+                        className="cart-item__link"
+                        href={sourceUrl}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Check Online
+                    </a>
+                    <p className="cart-item__quantity">Quantity: {quantity}</p>
+                </div>
+            </div>
+
+            <h4 className="cart-item__title--ingredients">Ingredients:</h4>
+            <ul className="cart-item__list--ingredients">
+                {ingredientsToRender &&
+                    ingredientsToRender.length > 1 &&
+                    ingredientsToRender.map((ing) => (
+                        <CartIngredientItem key={ing.id} ingredient={ing} />
+                    ))}
+            </ul>
+        </li>
+    );
+};
+export default CartItem;
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,13 +1,33 @@
-import { Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+
+import CartItem from 'components/cart/CartItem';
+import { useAppSelector } from 'hooks/useRedux';
 
 import './Cart.scss';
 
-import React from 'react';
-
 interface ICartProps {}
 
-const NotFound: React.FC<ICartProps> = () => {
-    return <div className="cart">this is cart page</div>;
+const Cart: React.FC<ICartProps> = () => {
+    const cart = useAppSelector((state) => state.ordersReducer.cart);
+
+    const cartItems = Object.values(cart);
+
+    return (
+        <div className="cart">
+            <header className="cart__header">
+                <h2>Your Cart</h2>
+            </header>
+            <div className="cart__container">
+                <ul className="cart__list">
+                    {cartItems &&
+                        cartItems.length > 1 &&
+                        cartItems.map((cartItem) => (
+                            <CartItem key={cartItem.id} item={cartItem} />
+                        ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
-export default NotFound;
+export default Cart;

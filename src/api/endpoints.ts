d808a5e03416ca6178b0a30cba17b1b1e6e8f177
urke90 <urosbijelic90@gmail.com>
2022-09-14 15:41:10 +0200

    init logic for adding cart items to DB WIP, check now update func to see if we can have just 1 single array

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -2,7 +2,15 @@ interface IAPI_Endpoints {
     [key: string]: string;
 }
 
-export const API_ENDPOINTS: IAPI_Endpoints = {
+const DB_BASE_ENDPOINT =
+    'https://pizza-order-6d2f9-default-rtdb.firebaseio.com/';
+
+export const PIZZA_ENDPOINTS: IAPI_Endpoints = {
     pizzas: 'https://forkify-api.herokuapp.com/api/search?q=pizza',
     pizzaId: 'https://forkify-api.herokuapp.com/api/get?rId='
+};
+
+export const DB_ENDPOINTS = {
+    users: DB_BASE_ENDPOINT + 'users',
+    orders: DB_BASE_ENDPOINT + 'orders'
 };

faee42f10134e2c4cb91e77e5975e6c7385c041c
urke90 <urosbijelic90@gmail.com>
2022-10-02 18:08:56 +0200

    refactor orderitem comp to show price etc grid layout

--- src/components/orders/OrderItem.tsx
+++ src/components/orders/OrderItem.tsx
@@ -1,4 +1,5 @@
 import { IOrderItem } from 'ts/orders-cart';
+import { countPizzaTotalPrice } from 'util/pizzas-data';
 
 import './OrderItem.scss';
 
@@ -7,33 +8,29 @@ interface IOrderItemProps {
 }
 
 const OrderItem: React.FC<IOrderItemProps> = ({ order }) => {
-    const { imageUrl, sourceUrl, title, quantity, address, ingredients } =
-        order;
-    // console.log('individual order', order);
+    const { imageUrl, sourceUrl, title, quantity, ingredients, price } = order;
+    console.log('individual order', order);
+
+    const totalPrice = countPizzaTotalPrice(price, quantity);
 
     return (
         <li className="order-item">
-            <header className="order-item__header">
-                <div className="cart-item__img">
-                    <img src={imageUrl} alt={title} />
-                </div>
-                <div className="order-item__description">
-                    <h5 className="order-item__title ellipsis">{title}</h5>
-                    <a
-                        className="order-item__link"
-                        href={sourceUrl}
-                        target="_blank"
-                        rel="noreferrer noopener"
-                    >
-                        Check Online
-                    </a>
-                    <div className="order-item__quantity">
-                        <p className="order-item__value">
-                            Quantity: {quantity}
-                        </p>
-                    </div>
-                </div>
-            </header>
+            <div className="order-item__img">
+                <img src={imageUrl} alt={title} />
+            </div>
+            <div className="order-item__description">
+                <h5 className="order-item__title ellipsis">{title}</h5>
+                <a
+                    className="order-item__link"
+                    href={sourceUrl}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
+                    Check Online
+                </a>
+                <p className="order-item__value">Quantity: {quantity}</p>
+                <p className="order-item__value">Total: {totalPrice} $</p>
+            </div>
         </li>
     );
 };
--- src/components/orders/OrderList.tsx
+++ src/components/orders/OrderList.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import { IOrder } from 'ts/orders-cart';
 
 import OrderItem from './OrderItem';
-import './OrderList';
+import './OrderList.scss';
 
-interface IOrdersListProps {}
+interface IOrdersListProps {
+    orderItem: IOrder;
+}
 
-const OrderList: React.FC<IOrdersListProps> = () => {
-    return <ul className="order-list">orders list</ul>;
+const OrderList: React.FC<IOrdersListProps> = ({ orderItem }) => {
+    const { items, totalPrice } = orderItem;
+
+    console.log('orderItem in OrderList.tsx', orderItem);
+
+    return (
+        <ul className="order-list">
+            {items.length > 0 &&
+                items.map((item) => (
+                    <OrderItem key={item.pizzaId} order={item} />
+                ))}
+        </ul>
+    );
 };
+
 export default OrderList;

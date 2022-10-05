837ecb502f1a986865e5814a93951401120c2a4d
urke90 <urosbijelic90@gmail.com>
2022-10-05 19:25:32 +0200

    created entries based on address object so we could map address

--- src/components/addresses/AddressItem.tsx
+++ src/components/addresses/AddressItem.tsx
@@ -14,19 +14,15 @@ interface IAddressItemProps {
     onAddressUpdate: (type: TAddressMode, addressId: string) => void;
 }
 
-/**
- * FIGURE OUT A WAY TO RENDER DIVS WITH ADDRESS INFO DYNAMICALLY
- */
-
 const AddressItem: React.FC<IAddressItemProps> = (props) => {
-    const { id, city, street, zipCode, floor, apartment, phone } =
-        props.address;
-
-    console.log('address', props.address);
-
-    const { onAddressUpdate, uid } = props;
     const dispatch = useAppDispatch();
     const isBtnDisabled = useAppSelector(addressesSelector.isBtnDisabled);
+
+    const { id } = props.address;
+    const { onAddressUpdate, uid } = props;
+
+    // Array of arrays where [0] === apartment/city/street... [1] === values
+    const addressToRender = Object.entries(props.address);
 
     const deleteAddressHandler = () => {
         dispatch(asyncDeleteAddress({ uid, addressId: id }));
@@ -34,31 +30,13 @@ const AddressItem: React.FC<IAddressItemProps> = (props) => {
 
     return (
         <li className="address-item">
-            <div className="address-item__data">
-                <h5 className="address-item__title">City</h5>
-                <p className="address-item__description">{street}</p>
-            </div>
-            <div className="address-item__data">
-                <h5 className="address-item__title">Street</h5>
-                <p className="address-item__description">{city}</p>
-            </div>
-            <div className="address-item__data">
-                <h5 className="address-item__title">Zip/Postal code</h5>
-                <p className="address-item__description">{zipCode}</p>
-            </div>
-            <div className="address-item__data">
-                <h5 className="address-item__title">Floor</h5>
-                <p className="address-item__description">{floor}</p>
-            </div>
-            <div className="address-item__data">
-                <h5 className="address-item__title">Apartment</h5>
-                <p className="address-item__description">{apartment}</p>
-            </div>
-            <div className="address-item__data">
-                <h5 className="address-item__title">Phone</h5>
-                <p className="address-item__description">{phone}</p>
-            </div>
-
+            {addressToRender.length > 0 &&
+                addressToRender.map(([label, value]) => (
+                    <div key={label} className="address-item__data">
+                        <h5 className="address-item__title">{label}</h5>
+                        <p className="address-item__description">{value}</p>
+                    </div>
+                ))}
             <div className="address-item__actions">
                 <Button
                     type="button"
